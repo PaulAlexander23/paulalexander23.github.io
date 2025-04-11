3a1cd66407b69e7cc445b976848891a632f9943e
Paul Alexander <paulalexander23@live.co.uk>
2025-04-11 16:00:09 +0100

    Add a script to view the webpages and update the makefile.

--- Makefile
+++ Makefile
@@ -1,5 +1,13 @@
 
+.PHONY: view check-for-updates
+
+view: Gemfile.lock
+	./serve.sh
+
 Gemfile.lock: Gemfile 
 	bundle install
+
+check-for-updates:
+	bundle update github-pages
 
 
---
+++ serve.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Start the Jekyll server
+bundle exec jekyll serve &
+
+# Wait a couple seconds to give the server time to start
+sleep 2
+
+# Open default browser to localhost
+URL="http://localhost:4000"
+
+# Check OS and open browser
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    xdg-open "$URL"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    open "$URL"
+else
+    echo "Unsupported OS. Please open $URL manually."
+fi
+
